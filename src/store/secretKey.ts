7e33092b6dea2a3e76ee8b74d5619b781f3c30e7
Tom Williams <tomwilliams@Toms-MacBook-Pro-2.local>
2023-08-19 23:46:07 -0700

    random firebase stuff

---
+++ src/store/secretKey.ts
@@ -0,0 +1,13 @@
+import { useRef } from "react";
+import { v4 } from "uuid";
+
+export function useSecretKey() {
+  const currentSecretKey = useRef<string | null>(
+    localStorage.getItem("secretKey")
+  );
+  if (!currentSecretKey.current) {
+    currentSecretKey.current = v4();
+    localStorage.setItem("secretKey", currentSecretKey.current);
+  }
+  return currentSecretKey.current;
+}
