510eda7aae0e374b385e4718a4138abfa7cc4b66
vermentor <nemeth.alex@protonmail.com>
2023-09-02 15:46:46 -0700

    Make PlayerRole prettier

--- src/PlayerLanding/PlayerRole.tsx
+++ src/PlayerLanding/PlayerRole.tsx
@@ -10,21 +10,61 @@ interface PlayerRoleProps {
 
 function PlayerRole({ self, characters }: PlayerRoleProps) {
   return (
-    <Flex direction="column" align="center">
-      <span>Tap to reveal your role.</span>
-      <Flex direction="column" align="center" className="role">
+    <Flex direction="column" align="center" className="role">
+      <Flex direction="column" align="center">
+        <div className="role-inner">
+          <div className="role-hidden">
+            <img
+              width={"300px"}
+              height={"300px"}
+              src="./src/assets/token_logo.png"
+              onContextMenu={(event) => {
+                event.preventDefault();
+                event.stopPropagation();
+              }}
+            />
+          </div>
+          <div className="role-shown">
+            <img
+              width={"300px"}
+              height={"300px"}
+              src="./src/assets/token_blank.png"
+              onContextMenu={(event) => {
+                event.preventDefault();
+                event.stopPropagation();
+              }}
+            />
+          </div>
+          <div className="role-shown">
+            <img
+              width={"300px"}
+              height={"300px"}
+              src={
+                characters.find(({ name }) => name === self.role)?.imageSrc ??
+                "./src/assets/default_role.svg"
+              }
+              onContextMenu={(event) => {
+                event.preventDefault();
+                event.stopPropagation();
+              }}
+            />
+            <div>{self.role}</div>
+          </div>
+        </div>
+      </Flex>
+      <div className="fingerprint">
         <img
-          src={
-            characters.find(({ name }) => name === self.role)?.imageSrc ??
-            "./src/assets/default_role.svg"
-          }
+          width={"85px"}
+          height={"110px"}
+          //style={{ paddingTop: "100%" }}
           onContextMenu={(event) => {
             event.preventDefault();
             event.stopPropagation();
           }}
+          src="./src/assets/fingerprint.png"
         />
-        <span>{self.role}</span>
-      </Flex>
+        <div>Hold to reveal role</div>
+      </div>
     </Flex>
   );
 }
