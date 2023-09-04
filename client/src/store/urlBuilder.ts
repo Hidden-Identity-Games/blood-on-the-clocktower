ca3f16352c97a1f30c3237e93b0adc1213380840
Tom Williams <tomwilliams@Toms-MacBook-Pro-2.local>
2023-09-04 14:41:18 -0700

    fix ws again

--- client/src/store/urlBuilder.ts
+++ client/src/store/urlBuilder.ts
@@ -1,7 +1,7 @@
 export function apiUrl(path: `/${string}`, options?: { websocket?: boolean }) {
   const url = new URL(`./api${path}`, window.location.origin);
   if (options?.websocket) {
-    url.protocol = window.location.protocol === "https" ? "wss" : "ws";
+    url.protocol = window.location.protocol.startsWith("https") ? "wss" : "ws";
   }
   return url.toString();
 }
