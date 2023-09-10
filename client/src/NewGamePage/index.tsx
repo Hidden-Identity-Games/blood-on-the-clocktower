65c3765f25940e65fa5e5ec7a8402146ddcb8ad5
anemeth <nemeth.alex@protonmail.com>
2023-09-09 18:15:29 -0700

    Start making startpage prettier

--- client/src/NewGamePage/index.tsx
+++ client/src/NewGamePage/index.tsx
@@ -1,16 +1,89 @@
+import {
+  Button,
+  Callout,
+  Dialog,
+  DialogClose,
+  Flex,
+  TextField,
+} from "@radix-ui/themes";
 import { useCreateGame } from "../store/useStore";
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
 export function NewGameLanding() {
   const [error, isLoading, , createGame] = useCreateGame();
+  const [joinCode, setJoinCode] = React.useState("");
+  const navigate = useNavigate();
 
   return (
-    <div>
-      {error && <div>error</div>}
-      {!isLoading ? (
-        <button onClick={() => createGame()}>Create Game</button>
-      ) : (
-        <div>Creating Game please wait</div>
+    <div
+      className="flex flex-col bg-contain bg-center"
+      style={{
+        // backgroundImage: `url(${background})`,
+        height: "100%",
+        width: "100%",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
+      {error && (
+        <div className="relative h-0 w-full">
+          <Callout.Root className="absolute bottom-0">
+            <Callout.Text>{error}</Callout.Text>
+          </Callout.Root>
+        </div>
       )}
+      <div className="w-full flex-1" />
+      <Flex
+        className="mb-3 w-full "
+        direction="row"
+        align="center"
+        gap="4"
+        justify={"center"}
+      >
+        <Button onClick={() => createGame()} disabled={isLoading}>
+          Create Game
+        </Button>
+
+        <Dialog.Root>
+          <Dialog.Trigger>
+            <Button disabled={isLoading}>Join Game</Button>
+          </Dialog.Trigger>
+          <Dialog.Content>
+            <Dialog.Title align="center">
+              <label htmlFor="game-code-input">Game code:</label>
+            </Dialog.Title>
+
+            <TextField.Input
+              id="game-code-input"
+              className="rounded"
+              value={joinCode}
+              onChange={(event) =>
+                setJoinCode(event.currentTarget.value.toUpperCase())
+              }
+              onKeyDown={(event) => {
+                if (event.key === "Enter") {
+                  navigate(`/${joinCode.toUpperCase()}`);
+                }
+              }}
+            />
+
+            <Flex justify="between" mt="3">
+              <DialogClose>
+                <Button>Cancel</Button>
+              </DialogClose>
+              <DialogClose>
+                <Button
+                  onClick={() => {
+                    navigate(`/${joinCode.toUpperCase}`);
+                  }}
+                >
+                  Join
+                </Button>
+              </DialogClose>
+            </Flex>
+          </Dialog.Content>
+        </Dialog.Root>
+      </Flex>
     </div>
   );
 }
