ba0bc29f1139a252182918fadf65db77e64306e3
anemeth <nemeth.alex@protonmail.com>
2023-09-22 14:52:56 -0700

    Store script remote, nullable getFile

--- server/src/database/remoteStorage.ts
+++ server/src/database/remoteStorage.ts
@@ -13,7 +13,7 @@ import {
 export interface IStorageObject<T> {
   listDirectories: () => Promise<string[]>
   listFiles: () => Promise<string[]>
-  getFile: () => Promise<T>
+  getFile: () => Promise<T | null>
   putFile: (object: T) => Promise<void>
 }
 
@@ -30,14 +30,14 @@ export class StorageObject<T> implements IStorageObject<T> {
 
   listDirectories: () => Promise<string[]> = async () => await this.storage.listDirectories()
   listFiles: () => Promise<string[]> = async () => await this.storage.listFiles(this.directory)
-  getFile: () => Promise<T> = async () => await this.storage.getFile(this.directory, this.file)
+  getFile: () => Promise<T | null> = async () => await this.storage.getFile(this.directory, this.file)
   putFile: (object: T) => Promise<void> = async (object: T) => { await this.storage.putFile(this.directory, this.file, object) }
 }
 
 interface IStorage {
   listDirectories: () => Promise<string[]>
   listFiles: (directory: string) => Promise<string[]>
-  getFile: <T>(directory: string, file: string) => Promise<T>
+  getFile: <T>(directory: string, file: string) => Promise<T | null>
   putFile: <T>(directory: string, file: string, object: T) => Promise<void>
 }
 
@@ -66,13 +66,13 @@ export class RemoteStorage implements IStorage {
     throw new Error('Remote storage error on listObjects')
   }
 
-  getFile = async <T>(directory: string, file: string): Promise<T> => {
+  getFile = async <T>(directory: string, file: string): Promise<T | null> => {
     const response = await this.storage.getObject(directory, file)
     if (response.$metadata.httpStatusCode === 200 && response.Body) {
       return response.Body as T
     }
 
-    throw new Error('Remote storage error on getObject')
+    return null
   }
 
   putFile = async <T>(directory: string, file: string, object: T): Promise<void> => {
