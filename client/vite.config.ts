ca85562329ab97ba0bdc3698b5f5631519595b1e
Tom Williams <tomwilliams@Toms-MacBook-Pro-2.local>
2023-10-03 22:01:10 -0700

    run tests

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,13 +1,20 @@
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
+
 import react from "@vitejs/plugin-react";
 import basicSsl from "@vitejs/plugin-basic-ssl";
+const TEST_MODE: boolean = !!process.env.TEST_MODE;
 const SERVER_PORT: number = Number(process.env.SERVER_PORT);
 const CLIENT_PORT: number = Number(process.env.CLIENT_PORT ?? 3000);
 console.log(`SERVER ON: ${SERVER_PORT}`);
 console.log(`CLIENT ON: ${CLIENT_PORT}`);
+console.log(`TEST_MODE: ${TEST_MODE}`);
 
 const HTTPS = true;
 export default defineConfig((env) => ({
+  test: {
+    setupFiles: "./test/testSetup.tsx",
+    environment: "jsdom",
+  },
   plugins: [react(), HTTPS && basicSsl()],
   build: {
     minify: false,
@@ -29,6 +36,8 @@ export default defineConfig((env) => ({
     WS_URL:
       env.command === "build"
         ? process.env.SERVER_URL ?? '"blood-on-the-clocktower.onrender.com"'
+        : TEST_MODE
+        ? "''"
         : `\`\${window.location.hostname}:${CLIENT_PORT}/api\``,
   },
 }));
