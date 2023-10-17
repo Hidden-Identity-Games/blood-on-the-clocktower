6ab72e3b8f38f58fe161d38a2398f099a95349ab
Alex Nemeth <80649445+vermentor@users.noreply.github.com>
2023-10-17 14:44:03 -0700

    Add QR Code dialog to game header (#152)

--- client/src/shared/GameHeader.tsx
+++ client/src/shared/GameHeader.tsx
@@ -1,26 +1,20 @@
-import { Callout, Flex, Heading, Link, Text } from "@radix-ui/themes";
+import {
+  Callout,
+  Dialog,
+  Flex,
+  Heading,
+  IconButton,
+  Link,
+  Text,
+} from "@radix-ui/themes";
 import { useGame } from "../store/GameContext";
 import { BsShare } from "react-icons/bs";
+import { AiOutlineClose } from "react-icons/ai";
 import { Link as ReactRouterLink } from "react-router-dom";
+import QRCode from "qrcode.react";
 
 export function GameHeader() {
   const { gameId, game } = useGame();
-  const url = `${document.location.protocol}//${document.location.hostname}${
-    document.location.port ? `:${document.location.port}` : ""
-  }/${gameId}`;
-  const handleShare = async () => {
-    if (navigator.share) {
-      try {
-        await navigator.share({ url, title: url, text: url });
-      } catch (error) {
-        console.log("Failed to share.");
-      }
-    } else {
-      navigator.clipboard.writeText(url);
-      // setShowSnackbar(true);
-      // setTimeout(() => setShowSnackbar(false), 2000);
-    }
-  };
 
   return (
     <>
@@ -31,14 +25,9 @@ export function GameHeader() {
         align="center"
         className="border-b-2 border-red-900"
       >
-        <Link
-          onClick={handleShare}
-          size="2"
-          className="flex-1 whitespace-nowrap"
-        >
-          <Text color="amber">Game:</Text> {gameId}
-          <BsShare className="ml-.5 inline text-xs" />
-        </Link>
+        <Flex gap="1" align="center">
+          <QRCodeDialog gameId={gameId} />
+        </Flex>
         <Heading size="2" className="shrink truncate capitalize">
           Status: {game?.gameStatus}
         </Heading>
@@ -55,5 +44,54 @@ export function GameHeader() {
         </Callout.Root>
       )}
     </>
+  );
+}
+
+interface QRCodeDialogProps {
+  gameId: string | null;
+}
+function QRCodeDialog({ gameId }: QRCodeDialogProps) {
+  const url = `${document.location.protocol}//${document.location.hostname}${
+    document.location.port ? `:${document.location.port}` : ""
+  }/${gameId}`;
+
+  const handleShare = async () => {
+    if (navigator.share) {
+      try {
+        await navigator.share({ url, title: url, text: url });
+      } catch (error) {
+        console.error("Failed to share.");
+      }
+    } else {
+      navigator.clipboard.writeText(url);
+    }
+  };
+
+  return (
+    <Dialog.Root>
+      <Dialog.Trigger>
+        <Link onClick={handleShare} size="2" className="whitespace-nowrap">
+          <Text color="amber">Game:</Text> {gameId}
+          <BsShare className="ml-.5 inline text-xs" />
+        </Link>
+      </Dialog.Trigger>
+
+      <Dialog.Content className="m-6">
+        <Dialog.Title>
+          <Flex align="center" justify="between">
+            Scan to Join
+            <Dialog.Close>
+              <IconButton variant="surface">
+                <AiOutlineClose />
+              </IconButton>
+            </Dialog.Close>
+          </Flex>
+        </Dialog.Title>
+
+        <Flex mt="4" justify="center">
+          <QRCode value={url} size={256} fgColor="darkred" />
+        </Flex>
+      </Dialog.Content>
+    </Dialog.Root>
   );
 }
