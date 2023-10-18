744c954b31e27de94596541aa667c3a8be769024
vermentor <nemeth.alex@protonmail.com>
2023-10-18 10:32:52 -0700

    First pass for adding characters to script

--- client/src/GamemasterInGame/CharacterSelectList.tsx
+++ client/src/GamemasterInGame/CharacterSelectList.tsx
@@ -1,18 +1,22 @@
 import { useMemo, useState } from "react";
-import { CharacterType, CharacterTypes } from "../types/script";
+import { CharacterType } from "../types/script";
 import {
   Checkbox,
   Flex,
   IconButton,
-  Select,
+  Text,
   TextField,
   Heading,
+  Dialog,
+  Separator,
 } from "@radix-ui/themes";
 import React from "react";
 import { colorMap } from "../shared/CharacterTypes";
 import { RoleIcon, RoleName } from "../shared/RoleIcon";
-import { Role } from "@hidden-identity/server";
+import { Character, Role } from "@hidden-identity/server";
 import { getCharacter } from "../assets/game_data/gameData";
+import { characters } from "../assets/game_data/characterData";
+import Fuse from "fuse.js";
 
 interface StateContainer<T> {
   value: T;
@@ -55,23 +59,19 @@ export function CharacterSelectList({
   state: CharacterSelectState;
   readOnly: boolean;
 }) {
-  const [newCharacterName, setNewCharacterName] = useState("");
-  const [newCharacterTeam, setNewCharacterTeam] =
-    useState<CharacterType>("Townsfolk");
-
-  function addNewCharacter() {
-    if (!newCharacterName) {
-      return;
-    }
-    // Switch to a GUID
-    const newId = newCharacterName;
-    state.additionalCharacters.set((curr) => [...curr, newId as Role]);
-    state.selectedRoles.set((selectedroles) => ({
-      ...selectedroles,
-      [newId]: true,
-    }));
-    setNewCharacterName("");
-  }
+  // function addNewCharacter() {
+  //   if (!newCharacterName) {
+  //     return;
+  //   }
+  //   // Switch to a GUID
+  //   const newId = newCharacterName;
+  //   state.additionalCharacters.set((curr) => [...curr, newId as Role]);
+  //   state.selectedRoles.set((selectedroles) => ({
+  //     ...selectedroles,
+  //     [newId]: true,
+  //   }));
+  //   setNewCharacterName("");
+  // }
   const charactersByType: Record<CharacterType, Role[]> = useMemo(() => {
     const allCharacters = [
       ...state.characters,
@@ -110,7 +110,7 @@ export function CharacterSelectList({
             {characters.map((role) => (
               <Flex
                 gap="1"
-                align={"center"}
+                align="center"
                 key={role}
                 style={{ height: "2em" }}
                 asChild
@@ -134,42 +134,82 @@ export function CharacterSelectList({
             ))}
           </React.Fragment>
         ))}
-
-      <Flex align={"center"} gap="2" pt="2" pb="6">
-        <IconButton
-          type="button"
-          size="1"
-          onClick={addNewCharacter}
-          disabled={!newCharacterName}
-        >
-          +
-        </IconButton>
-        <TextField.Input
-          placeholder="Additional role"
-          value={newCharacterName}
-          onChange={(event) => setNewCharacterName(event.currentTarget.value)}
-          onKeyDown={(event) => {
-            if (event.key === "Enter") {
-              event.preventDefault();
-              addNewCharacter();
-            }
-          }}
-        />
-        <Select.Root
-          onValueChange={(value) => setNewCharacterTeam(value as CharacterType)}
-          defaultValue={newCharacterTeam}
-        >
-          <Select.Trigger variant="soft" color="gray" />
-          <Select.Content>
-            {CharacterTypes.map((type) => (
-              <Select.Item key={type} value={type}>
-                {type}
-              </Select.Item>
-            ))}
-          </Select.Content>
-        </Select.Root>
-      </Flex>
-      {/* <div className="mobile-spacer" style={{ height: 150, width: "100%" }} /> */}
+      <Separator size="4" />
+      <AddRole characterSelectState={state} />
     </Flex>
+  );
+}
+
+interface AddRoleProps {
+  characterSelectState: CharacterSelectState;
+}
+function AddRole({ characterSelectState }: AddRoleProps) {
+  const [searchTerm, setSearchTerm] = React.useState("");
+  const [results, setResults] = React.useState<Character[]>([]);
+  const fuse = React.useMemo(
+    () => new Fuse(characters, { keys: ["name"] }),
+    [],
+  );
+
+  return (
+    <Dialog.Root>
+      <Dialog.Trigger>
+        <Flex gap="5" align="center" style={{ height: "2em" }}>
+          <IconButton variant="soft" size="1">
+            +
+          </IconButton>
+          <label>Add a Role</label>
+        </Flex>
+      </Dialog.Trigger>
+
+      <Dialog.Content className="mx-3">
+        <Dialog.Title>Role Search:</Dialog.Title>
+
+        <Flex direction="column" gap="2">
+          <TextField.Input
+            placeholder="Find a role..."
+            value={searchTerm}
+            onChange={(e) => {
+              const nextSearchTerm = e.currentTarget.value;
+              setSearchTerm(nextSearchTerm);
+              setResults(
+                fuse
+                  .search(nextSearchTerm)
+                  .slice(0, 3)
+                  .map(({ item }) => getCharacter(item.id as Role)),
+              );
+            }}
+          />
+          {results.map((role) => (
+            <Dialog.Close key={role.id}>
+              <button
+                onClick={() =>
+                  characterSelectState.additionalCharacters.set((curr) => [
+                    ...curr,
+                    role.id,
+                  ])
+                }
+              >
+                <Flex
+                  justify="between"
+                  align="center"
+                  style={{ height: "3em" }}
+                >
+                  <Flex gap="2" align="center">
+                    <RoleIcon role={role.id} className="h-6" />
+                    <Text size="5" autoCapitalize="true">
+                      {RoleName(role.id)}
+                    </Text>
+                  </Flex>
+                  <Text size="5" autoCapitalize="true">
+                    {role.team}
+                  </Text>
+                </Flex>
+              </button>
+            </Dialog.Close>
+          ))}
+        </Flex>
+      </Dialog.Content>
+    </Dialog.Root>
   );
 }
