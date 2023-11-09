67ac17e6715876294488c492ba201b17b3e5e9e0
vermentor <nemeth.alex@protonmail.com>
2023-11-09 14:21:06 -0800

    Add storyteller desktop/mobile view url param override

--- client/src/GamemasterInGame/GamemasterLanding.tsx
+++ client/src/GamemasterInGame/GamemasterLanding.tsx
@@ -1,5 +1,5 @@
 import { useGame, useSetScript } from "../store/useStore";
-import { useParams } from "react-router-dom";
+import { useParams, useSearchParams } from "react-router-dom";
 import { GameProvider } from "../store/GameContextProvider";
 import { Lobby } from "./Lobby";
 import { ScriptSelect } from "./ScriptSelect";
@@ -15,6 +15,8 @@ import { DesktopView } from "../GamemasterDesktop/DesktopView";
 export function GameMasterRoot() {
   const { gameId, gmHash } = useParams();
   const [view] = useDesktopOrMobile();
+  const [search] = useSearchParams();
+  const displayMode = search.get("display") ?? view;
 
   return (
     <Theme
@@ -32,10 +34,10 @@ export function GameMasterRoot() {
       }
     >
       <GameProvider gameId={gameId!}>
-        {view === "mobile" ? (
-          <GamemasterLanding providedGMHash={gmHash!} />
-        ) : (
+        {displayMode === "desktop" ? (
           <DesktopView isPlayerView={false} />
+        ) : (
+          <GamemasterLanding providedGMHash={gmHash!} />
         )}
       </GameProvider>
     </Theme>
