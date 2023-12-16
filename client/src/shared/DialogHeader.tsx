c9b2f6e2cd2208115fcd9fc618d3bf3bd3584009
Brandon <withrobm@gmail.com>
2023-12-15 21:42:53 -0800

    modified to allow Dialog.Title to be centered

--- client/src/shared/DialogHeader.tsx
+++ client/src/shared/DialogHeader.tsx
@@ -4,7 +4,9 @@ import { Dialog, Flex, IconButton } from "@radix-ui/themes";
 export function DialogHeader(props: { children: React.ReactNode }) {
   return (
     <Flex className="w-full" justify={props.children ? "between" : "end"}>
-      {props.children && <Dialog.Title>{props.children}</Dialog.Title>}
+      {props.children && (
+        <Dialog.Title className="flex-1">{props.children}</Dialog.Title>
+      )}
       <Dialog.Close>
         <IconButton variant="ghost" radius="full" size="1" className="w-fit">
           <Cross1Icon />
