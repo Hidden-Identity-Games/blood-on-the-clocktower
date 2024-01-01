cddec7812eb9eee59b89aec414f4ce3cf1ce8693
Alex Nemeth <nemeth.alex@protonmail.com>
2024-01-01 15:20:43 -0800

    Add day reminders items to gm list

--- client/src/shared/Reminders/ReminderIcon.tsx
+++ client/src/shared/Reminders/ReminderIcon.tsx
@@ -9,7 +9,7 @@ interface ReminderIconProps {
 export function ReminderIcon({ reminder, role }: ReminderIconProps) {
   return (
     <div className="relative">
-      <div className="absolute left-[50%] top-[50%] -translate-x-1/2 -translate-y-1/2 text-ellipsis text-base">
+      <div className="absolute left-[50%] top-[50%] -translate-x-1/2 -translate-y-1/2 text-ellipsis text-base capitalize">
         {reminder.name}
       </div>
       {reminder.fromPlayer && (
