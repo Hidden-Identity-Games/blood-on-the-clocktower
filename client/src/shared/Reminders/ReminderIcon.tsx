84929fba59b4056b7b32aa3daa3338924f2fcf50
Alex Nemeth <nemeth.alex@protonmail.com>
2024-01-01 13:16:48 -0800

    Color code reminders on GMTile

--- client/src/shared/Reminders/ReminderIcon.tsx
+++ client/src/shared/Reminders/ReminderIcon.tsx
@@ -1,14 +1,33 @@
-import { type PlayerReminder, type Role } from "@hidden-identity/shared";
+import {
+  getAllReminders,
+  type PlayerReminder,
+  type ReminderType,
+  type Role,
+} from "@hidden-identity/shared";
+import classNames from "classnames";
 
 import { RoleIcon } from "../RoleIcon";
 
 interface ReminderIconProps {
   reminder: PlayerReminder;
   role: Role;
+  useReminderTypeColor?: boolean;
 }
-export function ReminderIcon({ reminder, role }: ReminderIconProps) {
+export function ReminderIcon({
+  reminder,
+  role,
+  useReminderTypeColor = false,
+}: ReminderIconProps) {
+  const reminderData = getAllReminders()[reminder.name];
+  const typeColor = getReminderColorByType(reminderData.type);
+
   return (
-    <div className="relative">
+    <div
+      className={classNames(
+        "relative rounded-full",
+        useReminderTypeColor && typeColor,
+      )}
+    >
       <div className="absolute left-[50%] top-[50%] -translate-x-1/2 -translate-y-1/2 text-ellipsis text-base capitalize">
         {reminder.name}
       </div>
@@ -17,4 +36,17 @@ export function ReminderIcon({ reminder, role }: ReminderIconProps) {
       )}
     </div>
   );
+}
+
+function getReminderColorByType(type: ReminderType) {
+  switch (type) {
+    case "drunk":
+    case "poison":
+    case "abilitySpent":
+      return "bg-red-400";
+    case "protected":
+      return "bg-blue-400";
+    default:
+      return null;
+  }
 }
