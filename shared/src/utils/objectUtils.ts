a5fd0bf8b1c52ef4f33f9201e3d89950e0576d1c
Tom Williams <tomwilliams@Toms-MacBook-Pro-2.local>
2024-01-02 19:19:59 -0800

    fix kick player

--- shared/src/utils/objectUtils.ts
+++ shared/src/utils/objectUtils.ts
@@ -18,9 +18,6 @@ export function removeKey<
   Key extends string | number,
   Obj extends Record<Key, unknown>,
 >(obj: Obj, key: Key): Omit<Obj, Key> {
-  if (!Reflect.has(obj, key)) {
-    throw new Error(`Removing key ${key} but wasn't found`);
-  }
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const { [key]: _, ...next } = obj;
   return next;
